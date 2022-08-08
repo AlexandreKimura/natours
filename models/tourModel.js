b9057f05d213da64c0884957517aac9381015fc6
Alexandre Kimura <alexandremkimura@hotmail.com>
2022-08-07 23:50:33 -0300

    Adding aggregate middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -100,9 +100,15 @@ tourSchema.pre(/^find/, function (next) {
 });
 
 tourSchema.post(/^find/, (docs, next) => {
-  console.log(this.start);
   console.log(`Query took ${Date.now() - this.start} ms`);
   //console.log(docs);
+  next();
+});
+
+// Aggregation middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  //console.log(this.pipeline());
   next();
 });
 
