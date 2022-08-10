af2c8ba4f2bed040ff803b47a6e8f05fc918d7ce
Alexandre Kimura <alexandremkimura@hotmail.com>
2022-08-09 21:31:26 -0300

    Adding duplicate errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -31,6 +31,12 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.keyValue.name;
+  const message = `Duplicate field value /${value}/. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -38,12 +44,15 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
+    // eslint-disable-next-line node/no-unsupported-features/es-syntax
     let error = { ...err };
 
     if (err.name === 'CastError') {
       error = handleCastErrorDB(error);
+    } else if (err.code === 11000) {
+      error = handleDuplicateFieldsDB(error);
     }
-    console.log(error);
+
     sendErrorProd(error, res);
   }
 };
