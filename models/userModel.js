c70a75de454da9a39f47c6cc960d79001da513f6
Alexandre Kimura <alexandremkimura@hotmail.com>
2022-08-09 23:39:09 -0300

    Validate passwords and hashs

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'The name is required!'],
@@ -24,9 +25,27 @@ const UserSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm a password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Password are not the same',
+    },
   },
 });
 
-const User = mongoose.model('User', UserSchema);
+userSchema.pre('save', async function (next) {
+  //Only run if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  //Cost 12
+  this.password = await bcryptjs.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
