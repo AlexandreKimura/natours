287f1c77b2a1c455b56ae717aed29c1d65699aad
Alexandre Kimura <alexandremkimura@hotmail.com>
2022-08-13 00:21:23 -0300

    Adding update user function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,15 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const { catchAsync } = require('../utils/catchAsync');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
@@ -9,6 +19,30 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     results: users.length,
     data: {
       users,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const user = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.json({
+    status: 'success',
+    data: {
+      user,
     },
   });
 });
