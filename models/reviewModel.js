dac4549d571c73c7822beb50078efcdef12f88c9
Alexandre Kimura <alexandremkimura@hotmail.com>
2022-08-15 21:40:37 -0300

    Reading with populate

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -36,6 +36,18 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: '-__v -passwordChangedAt',
+  });
+
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
