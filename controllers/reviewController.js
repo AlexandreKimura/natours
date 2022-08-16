a4a4a950e8ddcc978898575287f28c7716d22d80
Alexandre Kimura <alexandremkimura@hotmail.com>
2022-08-15 23:39:24 -0300

    Merging routes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -14,7 +14,6 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
-  console.log(req.params.tourId);
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
 
