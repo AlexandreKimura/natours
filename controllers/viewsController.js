7da955c450ab026bb0a8f2c412c5a8e111f0dfd4
Alexandre Kimura <alexandremkimura@hotmail.com>
2022-08-20 01:13:41 -0300

    Creating Overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,14 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const { catchAsync } = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+
   res.render('overview', {
     title: 'All tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.render('tour', {
