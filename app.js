6d8050169d2e01ff7690e7eb8ff87a57f8c54df3
Alexandre Kimura <alexandremkimura@hotmail.com>
2022-08-21 17:36:50 -0300

    Integration login part 1

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -27,8 +28,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
-      defaultSrc: ["'self'", 'blob:', 'https://*.mapbox.com'],
-      scriptSrc: ["'self'", 'https://*.mapbox.com', "'unsafe-inline'", 'blob:'],
+      defaultSrc: [
+        "'self'",
+        'blob:',
+        'https://*.mapbox.com',
+        'https://cdn.jsdelivr.net',
+      ],
+      scriptSrc: [
+        "'self'",
+        'https://*.mapbox.com',
+        'https://cdn.jsdelivr.net',
+        "'unsafe-inline'",
+        'blob:',
+      ],
     },
   })
 );
@@ -49,6 +61,7 @@ app.use('/api', limiter);
 
 //Body parser
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 //Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
