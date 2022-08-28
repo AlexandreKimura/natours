63efc11b3049a46c5b45878a66fc79068fb18e14
Alexandre Fukano <alexandremkimura@hotmail.com>
2022-08-28 03:05:59 -0300

    Resizing multiple images of tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -36,9 +36,28 @@ exports.uploadTourImages = upload.fields([
 // upload.single('image') --> req.file
 // upload.array('images', 5) --> req.files
 
-exports.resizeTourImages = (req, res, next) => {
+exports.resizeTourImages = catchAsync(async(req, res, next) => {
+
+  if(!req.files.imageCover || !req.files.images) return next()
+
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`
+  await sharp(req.files.imageCover[0].buffer).resize(2000, 1333).toFormat('jpeg').jpeg({
+    quality: 90
+  }).toFile(`public/img/tours/${req.body.imageCover}`)
+
+  req.body.images = []
+  await Promise.all(req.files.images.map(async (file, index) => {
+    const filename = `tour-${req.params.id}-${Date.now()}-${index + 1}.jpeg`
+
+    await sharp(file.buffer).resize(2000, 1333).toFormat('jpeg').jpeg({
+      quality: 90
+    }).toFile(`public/img/tours/${req.body.imageCover}`)
+
+    req.body.images.push(filename)
+  }))
+
   next()
-}
+})
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
