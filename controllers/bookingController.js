0ff289d9e4905f5e40a1ec29d3533ccb0b617805
Alexandre Fukano <alexandremkimura@hotmail.com>
2022-09-04 01:33:39 -0300

    Creating a new booking with stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,6 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
 const AppError = require('../utils/appError');
 const { catchAsync } = require('../utils/catchAsync');
 
@@ -11,7 +12,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourID}&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourID,
@@ -31,4 +32,18 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session
   })
+})
+
+exports.createBookingCheckout = catchAsync (async (req, res, next) => {
+  const { tour, user, price } = req.query 
+  
+  if(!tour && !user && !price) return next()
+
+  await Booking.create({
+    tour,
+    user,
+    price
+  })
+
+  res.redirect(req.originalUrl.split('?')[0])
 })
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
@@ -12,9 +13,10 @@ router.post(
   viewsController.updateUserData
 );
 
+router.get('/', bookingController.createBookingCheckout, authController.isLoggedIn, viewsController.getOverview);
+
 router.use(authController.isLoggedIn);
 
-router.get('/', viewsController.getOverview);
 router.get('/tour/:slug', viewsController.getTour);
 router.get('/login', viewsController.login);
 
